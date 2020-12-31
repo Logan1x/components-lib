4caa141642cdca463ef5f57e3e4d2d05e7d1feba
noob <sharmakhushal78@gmail.com>
2020-12-31 14:43:15 +0530

    components html completed with dark mode

---
+++ app.js
@@ -0,0 +1,5 @@
+function darkmode(){
+    document.querySelector("body").style.color = "white";
+    document.querySelector("body").style.backgroundColor= "#1F2937";
+    document.querySelector(".ow").style.color= "black";
+}
