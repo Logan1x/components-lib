9b76805378c71fa384028958eb80b2cee0ef7c11
noob <sharmakhushal78@gmail.com>
2020-12-31 14:55:32 +0530

    implemented dark mode to light mode transation

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-function darkmode(){
-    document.querySelector("body").style.color = "white";
-    document.querySelector("body").style.backgroundColor= "#1F2937";
-    document.querySelector(".ow").style.color= "black";
+function darkmode() {
+    var text = document.getElementById("btn-dm").innerText
+    // alert(text)
+    if (text == "dark mode") {
+        document.querySelector("body").style.color = "white";
+        document.querySelector("body").style.backgroundColor = "#1F2937";
+        document.querySelector(".ow").style.color = "black";
+        document.getElementById("btn-dm").innerText = "Light Mode"
+    } else {
+        document.querySelector("body").style.color = "Black";
+        document.querySelector("body").style.backgroundColor = "white";
+        document.querySelector(".ow").style.color = "Black";
+        document.getElementById("btn-dm").innerText = "Dark Mode"
+    }
+
+
 }
