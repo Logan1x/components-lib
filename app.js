a233b196ec4e85478fd20cd3f6a3073a0c69e8d1
noob <sharmakhushal78@gmail.com>
2021-01-01 13:39:30 +0530

    removed unnessesary lines

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-// function darkmode() {
-//     var text = document.getElementById("btn-dm").innerText
-//     // alert(text)
-//     if (text == "Dark Mode") {
-//         document.querySelector("body").style.color = "white";
-//         document.querySelector("body").style.backgroundColor = "#1F2937";
-//         document.querySelector(".ow").style.color = "black";
-//         document.getElementById("btn-dm").innerText = "Light Mode"
-//     } else {
-//         document.querySelector("body").style.color = "Black";
-//         document.querySelector("body").style.backgroundColor = "white";
-//         document.querySelector(".ow").style.color = "Black";
-//         document.getElementById("btn-dm").innerText = "Dark Mode"
-//     }
-
-
-// }
-
 var currentTheme = 'dark';
 
 function changeTheme() {
