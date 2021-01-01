45b319fb2c64e873acd78871cd567c0ff334a26c
noob <sharmakhushal78@gmail.com>
2021-01-01 13:36:11 +0530

    dark mode implemented

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-function darkmode() {
-    var text = document.getElementById("btn-dm").innerText
-    // alert(text)
-    if (text == "Dark Mode") {
-        document.querySelector("body").style.color = "white";
-        document.querySelector("body").style.backgroundColor = "#1F2937";
-        document.querySelector(".ow").style.color = "black";
-        document.getElementById("btn-dm").innerText = "Light Mode"
+// function darkmode() {
+//     var text = document.getElementById("btn-dm").innerText
+//     // alert(text)
+//     if (text == "Dark Mode") {
+//         document.querySelector("body").style.color = "white";
+//         document.querySelector("body").style.backgroundColor = "#1F2937";
+//         document.querySelector(".ow").style.color = "black";
+//         document.getElementById("btn-dm").innerText = "Light Mode"
+//     } else {
+//         document.querySelector("body").style.color = "Black";
+//         document.querySelector("body").style.backgroundColor = "white";
+//         document.querySelector(".ow").style.color = "Black";
+//         document.getElementById("btn-dm").innerText = "Dark Mode"
+//     }
+
+
+// }
+
+var currentTheme = 'dark';
+
+function changeTheme() {
+    document.body.classList.toggle('dark-mode');
+
+    if (currentTheme === 'dark') {
+        document.getElementById('toggleknop').innerHTML = '<i class="fas fa-sun" id="zon" style="color:#d8c658;"></i>';
+        currentTheme = 'sun';
     } else {
-        document.querySelector("body").style.color = "Black";
-        document.querySelector("body").style.backgroundColor = "white";
-        document.querySelector(".ow").style.color = "Black";
-        document.getElementById("btn-dm").innerText = "Dark Mode"
+        document.getElementById('toggleknop').innerHTML = '<i class="fas fa-moon" id="maan" style="color:#737eac;"></i>';
+        currentTheme = 'dark';
     }
-
-
 }
